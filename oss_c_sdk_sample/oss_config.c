f64629a9f3c267de80f71dac310e61c4cc3e1624
wanghai1988 <739659247@qq.com>
2017-09-08 10:12:26 +0800

    .
    .

---
+++ oss_c_sdk_sample/oss_config.c
@@ -0,0 +1,11 @@
+#include "oss_config.h"
+
+const char OSS_ENDPOINT[] = "oss-cn-beijing.aliyuncs.com";
+const char ACCESS_KEY_ID[] = "LTAI8UYryZAEjlZu";
+const char ACCESS_KEY_SECRET[] = "bJw7Bx21bzr9uIU4JO9565m22cvJB3";
+const char BUCKET_NAME[] = "qianan-flood";
+const char OBJECT_NAME[] = "dw10.1.exe";
+const char MULTIPART_UPLOAD_FILE_PATH[] = "<your local file>";
+
+//surfix must be '/', only used for dir relative sample
+const char DIR_NAME[] = "dir/"; 
